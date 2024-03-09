d451fc37fc83ce42c6577b7ffcff343d9a26b625
HarmonySpace <jchavaspace@gmail.com>
2024-03-09 11:49:49 -0600

    Update fonts Add 2 new formats in the project Create a font-face in layout to al fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,25 @@
 </template>
 
 <style>
+  @font-face {
+    font-family: "Chivo";
+    src: url("fonts/Chivo-Regular.woff2"),
+    url("fonts/Chivo-Regular.ttf");
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Chivo-Bold";
+    src: url("fonts/Chivo-ExtraBold.woff2"),
+    url("fonts/Chivo-ExtraBold.ttf");
+    font-display: swap;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Chivo;
+  }
   .page {
     width: 100%;
     display: flex;
