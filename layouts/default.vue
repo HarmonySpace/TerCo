b97ce081005d1928a1dd8e7826ea27cbe8ae5efd
HarmonySpace <jchavaspace@gmail.com>
2024-03-09 11:27:14 -0600

    move page styles move page container styles to default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="page">
+    <section class="limiter">
+      <slot />
+    </section>
+  </main>
+</template>
+
+<style>
+  .page {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: #eeeeee;
+  }
+  .limiter {
+    padding: 3rem;
+    width: 90%;
+    display: flex;
+    justify-content: center;
+  }
+</style>
