55fd9dbc27bb04b16e18b87b1e59c4115b6c43f8
HarmonySpace <jchavaspace@gmail.com>
2024-03-08 23:36:35 -0600

    Add nav elemets and styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,37 @@
+<template>
+  <main class="page">
+    <section class="limiter">
+      <nav class="nav">
+      <div class="nav__logo">Logo</div>
+      <div class="nav__links">
+        <a class="nav__link" href="#">Github</a>
+        <a class="nav__link" href="#">Acerda de</a>
+      </div>
+    </nav>
+    </section>
+  </main>
+</template>
 
+<style scoped>
+  .page {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .limiter {
+    width: 80%;
+    display: flex;
+    justify-content: center;
+  }
+  .nav {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo {
+    width: 100%;
+     
+  }
+
+</style>
