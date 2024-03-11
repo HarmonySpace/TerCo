f8860eb8e896ab59ade9676d8bb8244593b4d1a9
HarmonySpace <jchavaspace@gmail.com>
2024-03-11 14:51:32 -0600

    update button Add a unique selection button and update buttons style

---
+++ components/buttons/ButtonText.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(['text'])
+</script>
+
+<template>
+	<button class="outline-none border-none py-4 px-12 text-2xl bg-[var(--bg)] shadow-blue-200 shadow-xl rounded-xl transition-all duration-200 ease-out hover:px-14 active:bg-blue-200 focus:bg-blue-500 focus:text-blue-100 disabled:text-blue-100">
+		{{ props.text }}
+	</button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
+<script setup lang="ts">
+  /* buttons vars */
+  const btn_class1 = ref('bg-blue-500 text-blue-100')
+  const btn_class2 = ref('')
+  const btn_class3 = ref('')
+  /* buttons functions */
+  const resetBt = () => {
+    btn_class1.value = btn_class2.value = btn_class3.value = 'text-blue-900'
+  }
+  const selectedBt1 = () => {
+    resetBt()
+    btn_class1.value = 'bg-blue-500 text-blue-100'
+  }
+  const selectedBt2 = () => {
+    resetBt()
+    btn_class2.value = 'bg-blue-500 text-blue-100'
+  }
+  const selectedBt3 = () => {
+    resetBt()
+    btn_class3.value = 'bg-blue-500 text-blue-100'
+  }
+</script>
+
 <template>
-  <navBar />
-  <section class="p-48 w-full flex justify-center gap-8">
-    
+  <NavBar />
+  <section class="pt-48 w-full flex justify-start gap-8">
+    <ButtonsButtonText text="4 colores" :class="btn_class1" @click="selectedBt1" />
+    <ButtonsButtonText text="6 colores" :class="btn_class2" @click="selectedBt2" disabled />
+    <ButtonsButtonText text="10 colores" :class="btn_class3" @click="selectedBt3" disabled/>
   </section>
 </template>
