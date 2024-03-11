df1cb39d0fc78679f22013b3efd040bfb1abc966
HarmonySpace <jchavaspace@gmail.com>
2024-03-11 16:31:34 -0600

    update navs/buttons/index update componets names update button implement in index add Others/PalleteBar.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,29 @@
 <script setup lang="ts">
-  /* buttons vars */
-  const btn_class1 = ref('bg-blue-500 text-blue-100')
-  const btn_class2 = ref('')
-  const btn_class3 = ref('')
-  /* buttons functions */
-  const resetBt = () => {
-    btn_class1.value = btn_class2.value = btn_class3.value = 'text-blue-900'
+const normalClass = 'text-blue-900';
+const activeClass = 'bg-blue-500 text-blue-100';
+const selected_button = ref(0)
+const labels = [
+  {
+    id: 1,
+    text: "4 Colors"
+  },
+  {
+    id: 2,
+    text: "6 Colors"
+  },
+  {
+    id: 3,
+    text: "10 Colors"
   }
-  const selectedBt1 = () => {
-    resetBt()
-    btn_class1.value = 'bg-blue-500 text-blue-100'
-  }
-  const selectedBt2 = () => {
-    resetBt()
-    btn_class2.value = 'bg-blue-500 text-blue-100'
-  }
-  const selectedBt3 = () => {
-    resetBt()
-    btn_class3.value = 'bg-blue-500 text-blue-100'
-  }
+]
+const selectButton = (id: number) => {
+  selected_button.value = id
+}
 </script>
 
 <template>
-  <NavBar />
+  <NavbarsSimpleBar />
   <section class="pt-48 w-full flex justify-start gap-8">
-    <ButtonsButtonText text="4 colores" :class="btn_class1" @click="selectedBt1" />
-    <ButtonsButtonText text="6 colores" :class="btn_class2" @click="selectedBt2" disabled />
-    <ButtonsButtonText text="10 colores" :class="btn_class3" @click="selectedBt3" disabled/>
+    <ButtonsButtonText v-for="label in labels" :key="label.id" :text="label.text" :class="selected_button === label.id ? activeClass : normalClass" @click="selectButton(label.id)" />
   </section>
 </template>
