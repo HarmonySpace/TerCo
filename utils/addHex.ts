e6148928e27f83064fb19b7b878ce1e4ab2a1a10
HarmonySpace <jchavaspace@gmail.com>
2024-03-17 10:46:20 -0600

    add utils - add a addhex util - add a resthex util

---
+++ utils/addHex.ts
@@ -0,0 +1,5 @@
+export default (c1: string, c2:string) => {
+  let hexStr = (parseInt(c1, 16) + parseInt(c2, 16)).toString(16);
+  while (hexStr.length < 6) { hexStr = '0' + hexStr; }
+  return hexStr;
+}
