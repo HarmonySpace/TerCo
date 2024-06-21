62064194e883eaba334077776f77b80d779352e2
HarmonySpace <jchavaspace@gmail.com>
2024-06-21 11:32:38 -0600

    add a brighten-darken feature
    add buttons
    - brighten text colot
    - darken bg color

--- pages/index.vue
+++ pages/index.vue
@@ -57,6 +57,14 @@ const generate_background = (id: number) => {
   bg.value = colorGen();
   updateColor(id, bg.value);
 };
+const moreBright = (id: number) => {
+  fg.value = brightenColor(fg.value, 0.1);
+  updateColor(id, fg.value);
+};
+const moreDark = (id: number) => {
+  bg.value = darkenColor(bg.value, 0.1);
+  updateColor(id, bg.value);
+};
 </script>
 
 <template>
@@ -91,28 +99,17 @@ const generate_background = (id: number) => {
       >
         <template v-for="color in store.colors" key="color.id">
           <div
-            v-if="color.id === 6"
-            class="flex justify-between items-center gap-4 w-full"
-          >
-            <Icon
-              name="mdi:dice-3"
-              @click="generate_background(color.id)"
-              class="grow w-6 sm:w-8 h-8 sm:h-8 text-shark-500 transition-all duration-150 hover:text-shark-700 hover:cursor-pointer hover:scale-90"
-            />
-            <InputsSimpleInput
-              :id="'inputColor' + color.id"
-              :placeholder="color.placeholder"
-              :value="bg"
-              @getValue="(value) => updateColor(color.id, value)"
-            />
-          </div>
-          <div
-            v-else-if="color.id === 5"
+            v-if="color.id === 5"
             class="flex justify-between items-center gap-4 w-full"
           >
             <Icon
               name="mdi:dice-3"
               @click="generate_foreground(color.id)"
+              class="grow w-6 sm:w-8 h-8 sm:h-8 text-shark-500 transition-all duration-150 hover:text-shark-700 hover:cursor-pointer hover:scale-90"
+            />
+            <Icon
+              name="heroicons:arrow-up-16-solid"
+              @click="moreBright(color.id)"
               class="grow w-6 sm:w-8 h-8 sm:h-8 text-shark-500 transition-all duration-150 hover:text-shark-700 hover:cursor-pointer hover:scale-90"
             />
             <InputsSimpleInput
@@ -122,7 +119,27 @@ const generate_background = (id: number) => {
               @getValue="(value) => updateColor(color.id, value)"
             />
           </div>
-
+          <div
+            v-else-if="color.id === 6"
+            class="flex justify-between items-center gap-4 w-full"
+          >
+            <Icon
+              name="mdi:dice-3"
+              @click="generate_background(color.id)"
+              class="grow w-6 sm:w-8 h-8 sm:h-8 text-shark-500 transition-all duration-150 hover:text-shark-700 hover:cursor-pointer hover:scale-90"
+            />
+            <Icon
+              name="heroicons:arrow-down-16-solid"
+              @click="moreDark(color.id)"
+              class="grow w-6 sm:w-8 h-8 sm:h-8 text-shark-500 transition-all duration-150 hover:text-shark-700 hover:cursor-pointer hover:scale-90"
+            />
+            <InputsSimpleInput
+              :id="'inputColor' + color.id"
+              :placeholder="color.placeholder"
+              :value="bg"
+              @getValue="(value) => updateColor(color.id, value)"
+            />
+          </div>
           <div v-else>
             <InputsSimpleInput
               :id="'inputColor' + color.id"
