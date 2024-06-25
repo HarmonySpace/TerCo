e457b3c059c534c20c7be07c4c6b1b5515ca706d
HarmonySpace <jchavaspace@gmail.com>
2024-06-25 15:24:56 -0600

    update hexadecimal utils

--- utils/addHex.ts
+++ utils/addHex.ts
@@ -1,15 +1,15 @@
 export default (hex1: string, hex2: string) => {
-  let r1 = parseInt(hex1.slice(0,2), 16)
-  let g1 = parseInt(hex1.slice(2,4), 16)
-  let b1 = parseInt(hex1.slice(4,6), 16)
-  let r2 = parseInt(hex2.slice(0,2), 16)
-  let g2 = parseInt(hex2.slice(2,4), 16)
-  let b2 = parseInt(hex2.slice(4,6), 16)
-  let r = Math.min(255, r1 + r2)
-  let g = Math.min(255, g1 + g2)
-  let b = Math.min(255, b1 + b2)
+  const r1 = Number.parseInt(hex1.slice(0,2), 16)
+  const g1 = Number.parseInt(hex1.slice(2,4), 16)
+  const b1 = Number.parseInt(hex1.slice(4,6), 16)
+  const r2 = Number.parseInt(hex2.slice(0,2), 16)
+  const g2 = Number.parseInt(hex2.slice(2,4), 16)
+  const b2 = Number.parseInt(hex2.slice(4,6), 16)
+  const r = Math.min(255, r1 + r2)
+  const g = Math.min(255, g1 + g2)
+  const b = Math.min(255, b1 + b2)
 
-  let hex = ((r << 16) | (g << 8) | b).toString(16).padStart(6, 'f')
+  const hex = ((r << 16) | (g << 8) | b).toString(16).padStart(6, 'f')
 
   if (/^[0-9A-Fa-f]{6}$/.test(hex)) return hex;
   return 'ffffff'
--- utils/brightenColor.ts
+++ utils/brightenColor.ts
@@ -1,7 +1,7 @@
 export default (hex: string, percent: number) => {
-  let r = parseInt(hex.slice(0,2), 16)
-  let g = parseInt(hex.slice(2,4), 16)
-  let b = parseInt(hex.slice(4,6), 16)
+  let r = Number.parseInt(hex.slice(0,2), 16)
+  let g = Number.parseInt(hex.slice(2,4), 16)
+  let b = Number.parseInt(hex.slice(4,6), 16)
   r = Math.round(r * (1 + percent))
   g = Math.round(g * (1 + percent))
   b= Math.round(b* (1 + percent))
