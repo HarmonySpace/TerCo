f5b26cfe15926668b7c2a9bc32fa1ec7064f26ee
HarmonySpace <jchavaspace@gmail.com>
2024-06-25 15:27:47 -0600

    preformance update

--- utils/themeGen.ts
+++ utils/themeGen.ts
@@ -1,88 +1,10 @@
+import { useBrightStore } from "~/stores/Bright";
+
 export default () => {
 	const store = useColorsStore();
-	const bright:string = '101010'
-	const bright1:string = '202020'
-	const bright2:string = '303030'
-	const bright3:string = '404040'
+	const storeB = useBrightStore()
 	const theme =
-		"[colors]\n" +
-		'cursor = { text = "#' +
-		store.colors[4].value +
-		'", cursor = "#' +
-		store.colors[0].value +
-		'"}\n' +
-		'selection = { text = "#' +
-		store.colors[5].value +
-		'", cursor = "#' +
-		store.colors[0].value +
-		'"}\n' +
-		"[colors.primary]\n" +
-		'foreground = "#' +
-		store.colors[4].value +
-		'"\n' +
-		'background = "#' +
-		store.colors[5].value +
-		'"\n' +
-		"[colors.search]\n" +
-		'matches = { foreground = "#' +
-		store.colors[5].value +
-		'", background = "#' +
-		store.colors[0].value +
-		'"}\n' +
-		'focused_match = { foreground = "#' +
-		store.colors[5].value +
-		'", background = "#' +
-		store.colors[1].value +
-		'"}\n' +
-		"[colors.normal]\n" +
-		'black = "#' +
-		addHex(store.colors[5].value, bright) +
-		'"\n' +
-		'red = "#' +
-		store.colors[2].value +
-		'"\n' +
-		'green = "#' +
-		store.colors[1].value +
-		'"\n' +
-		'yellow = "#' +
-		store.colors[3].value +
-		'"\n' +
-		'blue = "#' +
-		store.colors[0].value +
-		'"\n' +
-		'magenta = "#' +
-		addHex(store.colors[2].value, bright2) +
-		'"\n' +
-		'cyan = "#' +
-		addHex(store.colors[0].value, bright2) +
-		'"\n' +
-		'white = "#' +
-		restHex(store.colors[4].value, bright1) +
-		'"\n' +
-		"[colors.bright]\n" +
-		'black = "#' +
-		addHex(store.colors[5].value, bright1) +
-		'"\n' +
-		'red = "#' +
-		addHex(store.colors[2].value, bright1) +
-		'"\n' +
-		'green = "#' +
-		addHex(store.colors[1].value, bright1) +
-		'"\n' +
-		'yellow = "#' +
-		addHex(store.colors[3].value, bright1) +
-		'"\n' +
-		'blue = "#' +
-		addHex(store.colors[0].value, bright1) +
-		'"\n' +
-		'magenta = "#' +
-		addHex(store.colors[2].value, bright3) +
-		'"\n' +
-		'cyan = "#' +
-		addHex(store.colors[0].value, bright3) +
-		'"\n' +
-		'white = "#' +
-		addHex(store.colors[4].value, bright2) +
-		'"';
+		`[colors]\ncursor = { text = "#${store.colors[4].value}", cursor = "#${store.colors[0].value}"}\nselection = { text = "#${store.colors[5].value}", cursor = "#${store.colors[0].value}"}\n[colors.primary]\nforeground = "#${store.colors[4].value}"\nbackground = "#${store.colors[5].value}"\n[colors.search]\nmatches = { foreground = "#${store.colors[5].value}", background = "#${store.colors[0].value}"}\nfocused_match = { foreground = "#${store.colors[5].value}", background = "#${store.colors[1].value}"}\n[colors.normal]\nblack = "#${addHex(store.colors[5].value, storeB.brights[0])}"\nred = "#${store.colors[2].value}"\ngreen = "#${store.colors[1].value}"\nyellow = "#${store.colors[3].value}"\nblue = "#${store.colors[0].value}"\nmagenta = "#${addHex(store.colors[2].value, storeB.brights[2])}"\ncyan = "#${addHex(store.colors[0].value, storeB.brights[2])}"\nwhite = "#${restHex(store.colors[4].value, storeB.brights[1])}"\n[colors.bright]\nblack = "#${addHex(store.colors[5].value, storeB.brights[1])}"\nred = "#${addHex(store.colors[2].value, storeB.brights[1])}"\ngreen = "#${addHex(store.colors[1].value, storeB.brights[1])}"\nyellow = "#${addHex(store.colors[3].value, storeB.brights[1])}"\nblue = "#${addHex(store.colors[0].value, storeB.brights[1])}"\nmagenta = "#${addHex(store.colors[2].value, storeB.brights[3])}"\ncyan = "#${addHex(store.colors[0].value, storeB.brights[3])}"\nwhite = "#${addHex(store.colors[4].value, storeB.brights[2])}"`;
+
 	return theme;
 };
