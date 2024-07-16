1d67b5c5b0359c4a954773c5e1f6c60a576dd860
HarmonySpace <jchavaspace@gmail.com>
2024-07-16 11:09:03 -0600

    > actualizado paleta de colores de los botones

--- components/Buttons/IconButton.vue
+++ components/Buttons/IconButton.vue
@@ -4,8 +4,8 @@ const props = defineProps(["icon"]);
 
 
 <template>
-  <div class="p-2 flex justify-center items-center bg-mercury-200 dark:bg-shark-800 border-mercury-300 dark:border-shark-700 border-2 rounded-lg right-8 transition-all duration-150 active:scale-90 hover:cursor-pointer">
-    <Icon :name="props.icon" size="1.5rem" class="text-mercury-500 dark:text-shark-500"></Icon>
+  <div class="p-2 flex justify-center items-center bg-mercury-300 dark:bg-shark-800 border-mercury-600 dark:border-shark-700 border-2 rounded-lg right-8 transition-all duration-150 active:scale-90 hover:cursor-pointer">
+    <Icon :name="props.icon" size="1.5rem" class="text-mercury-700 dark:text-shark-500"></Icon>
   </div>
 </template>
 
