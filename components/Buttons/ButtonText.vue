d58fa087e7cc1871bcd406b416857951d85c0af4
HarmonySpace <jchavaspace@gmail.com>
2024-08-03 10:51:21 -0600

    style: :art: update buttons components
    change a buttonText component

--- components/Buttons/ButtonText.vue
+++ components/Buttons/ButtonText.vue
@@ -4,7 +4,8 @@ const props = defineProps(["text"]);
 
 <template>
 	<button
-		class="outline-none border-none py-4 px-12 text-sm rounded-xl text-shark-900 bg-mercury-300 dark:text-shark-200 dark:bg-shark-900 transition-all duration-200 ease-out hover:bg-mercury-400 dark:hover:bg-shark-800 active:bg-mercury-400 dark:active:bg-shark-700 focus:text-mercury-900 focus:bg-mercury-500 dark:focus:text-shark-200 dark:focus:bg-shark-700 disabled:text-mercury-500 disabled:bg-mercury-200 dark:disabled:text-shark-700 dark:disabled:bg-shark-950">
-		{{ props.text }}
-	</button>
+	class="outline-none border-none py-4 px-6 text-lg rounded-xl transition-all duration-200 ease-out text-gray-900 bg-gray-50 hover:bg-gray-100 active:text-gray-900 active:bg-blue-100 focus:text-blue-50 focus:bg-blue-500 disabled:text-gray-300 disabled:bg-gray-200 dark:text-shark-200 dark:bg-shark-900 dark:hover:bg-shark-800 dark:active:bg-shark-700 dark:focus:text-shark-200 dark:focus:bg-shark-700 dark:disabled:text-shark-700 dark:disabled:bg-shark-950"
+	v-bind="$attrs">
+	{{ props.text }}
+</button>
 </template>
