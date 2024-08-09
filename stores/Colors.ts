6caa6b8b50cec90e8cb4b338b6c8a5763d02b413
HarmonySpace <jchavaspace@gmail.com>
2024-08-08 23:36:12 -0600

    refactor: change default theme
    update default theme in color store

--- stores/Colors.ts
+++ stores/Colors.ts
@@ -2,43 +2,43 @@ export const useColorsStore = defineStore("colors", () => {
 	const colors = ref([
 		{
 			id: 1,
-			value: "82A8D9",
+			value: "",
 			placeholder: "Color 1",
 		},
 		{
 			id: 2,
-			value: "6F87A6",
+			value: "",
 			placeholder: "Color 2",
 		},
 		{
 			id: 3,
-			value: "3F5573",
+			value: "",
 			placeholder: "Color 3",
 		},
 		{
 			id: 4,
-			value: "B2B1E6",
+			value: "",
 			placeholder: "Color 4",
 		},
 		{
 			id: 5,
-			value: "BBCDF2",
+			value: "",
 			placeholder: "Texto",
 		},
 		{
 			id: 6,
-			value: "222F40",
+			value: "",
 			placeholder: "Fondo",
 		},
 	]);
 	const file_name = ref("simple");
 	const colorsDefault = [
-		"82A8D9",
-		"6F87A6",
-		"3F5573",
-		"B2B1E6",
-		"BBCDF2",
-		"222F40",
+		"402e7a",
+		"4c3bcf",
+		"4b70f5",
+		"3dc2ec",
+		"8fd5ff",
+		"0d0a19",
 	];
 
 	onMounted(() => {
