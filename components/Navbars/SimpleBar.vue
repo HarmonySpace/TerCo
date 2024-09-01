ac35040cf0208f3d5365c598429dc3e4d5141787
HarmonySpace <jchavaspace@gmail.com>
2024-08-31 22:46:54 -0600

    fix(commit): error in commits

--- components/Navbars/SimpleBar.vue
+++ components/Navbars/SimpleBar.vue
@@ -19,36 +19,61 @@ const showMenu = ref(false);
     <div class="flex justify-between items-center w-full">
       <div class="h-8 flex justify-center items-center gap-8">
         <IconsLogo class="w-icon-1 md:h-12 md:w-12 fill-blue-500" />
-        <h1 class="text-3xl sm:text-7xl md:text-6xl font-normal text-gray-900 dark:text-gray-200">
+        <h1
+          class="text-3xl sm:text-7xl md:text-6xl font-normal text-gray-900 dark:text-gray-200"
+        >
           TerCo
         </h1>
       </div>
       <section class="flex justify-end items-center gap-4 sm:gap-8">
         <section class="flex justify-end items-center gap-12">
           <ButtonsToggleColorMode></ButtonsToggleColorMode>
-          <ButtonsIconButton class="hidden lg:flex w-icon-2 justify-center items-center" v-for="anchor in anchors"
-            :key="anchor.name" :href="anchor.url" :icon="anchor.icon">
+          <ButtonsIconButton
+            class="hidden lg:flex w-icon-2 justify-center items-center"
+            v-for="anchor in anchors"
+            :key="anchor.name"
+            :href="anchor.url"
+            :icon="anchor.icon"
+          >
           </ButtonsIconButton>
         </section>
-        <svg @click="
-          showMenu = !showMenu;
-        $emit('itsActive');
-        " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+        <svg
+          @click="
+            showMenu = !showMenu;
+            $emit('itsActive');
+          "
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
           class="w-6 sm:w-10 stroke-shark-900 dark:stroke-shark-500 lg:hidden transition-all duration-150 hover:cursor-pointer hover:scale-75"
-          :class="{ 'rotate-90': showMenu }">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          :class="{ 'rotate-90': showMenu }"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
         </svg>
       </section>
     </div>
     <Transition name="nav">
-      <div class="pt-32 flex flex-wrap justidy-start items-start w-full lg:hidden" v-show="showMenu">
-        <h1 class="w-full text-shark-900 dark:text-shark-300 text-5xl font-bold pb-8">
+      <div
+        class="pt-32 flex flex-wrap justidy-start items-start w-full lg:hidden"
+        v-show="showMenu"
+      >
+        <h1
+          class="w-full text-shark-900 dark:text-shark-300 text-5xl font-bold pb-8"
+        >
           Menú
         </h1>
         <ul class="flex flex-col">
           <li v-for="anchor in anchors">
-            <a class="w-full text-gray-500 dark:text-gray-700 text-start text-xl sm:text-3xl transition-all duration-250 hover:text-blue-500 dark:hover:text-blue-500 hover:translate-x-4"
-              :href="anchor.url">{{ anchor.name }}</a>
+            <a
+              class="w-full text-gray-500 dark:text-gray-700 text-start text-xl sm:text-3xl transition-all duration-250 hover:text-blue-500 dark:hover:text-blue-500 hover:translate-x-4"
+              :href="anchor.url"
+              >{{ anchor.name }}</a
+            >
           </li>
         </ul>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 useHead({
   title: "TerCo - Terminal Color",
+<<<<<<< HEAD
   meta: [
     {
       name: "A easy terminal color theme generator",
@@ -8,6 +9,9 @@ useHead({
     },
   ],
 })
+=======
+});
+>>>>>>> c2d23fd (refactor(interface): remove some feature)
 const store = useColorsStore();
 const fg = ref("");
 const bg = ref("");
@@ -21,18 +25,6 @@ const buttons = [
     text: "4",
     value: 6,
     disable: false,
-  },
-  {
-    id: 2,
-    text: "6",
-    value: 6,
-    disable: true,
-  },
-  {
-    id: 3,
-    text: "8",
-    value: 8,
-    disable: true,
   },
 ];
 const profiles = [
@@ -90,58 +82,121 @@ const copyCode = (id: number) => {
     copyThis.value = alacrittyThemeGen();
     copy(copyThis.value);
   }
-}
+};
 </script>
 
 <template>
   <section class="pt-32 flex flex-wrap justify-between w-full">
-    <div class="flex flex-wrap justify-center items-center gap-2 w-full h-full md:flex-nowrap">
-      <ButtonsButtonText class="grow basis-full md:basis-1/12" v-for="button in buttons" :key="button.id"
-        :text="button.text" @click="selectButton(button.id)" :disabled="button.disable" />
-      <InputsSimpleInput class="grow basis-full md:basis-auto" @getValue="(value) => updateFile(value)"
-        :placeholder="store.getFile()" :value="file" />
-      <ButtonsIconButton class="grow basis-full w-icon-1 my-8 md:basis-1/12 md:my-0"
-        icon="heroicons:arrow-path-16-solid" @click="resetColors()" />
+    <div
+      class="flex flex-wrap justify-start items-center gap-2 w-full h-full md:flex-nowrap"
+    >
+      <ButtonsButtonText
+        class="basis-full md:basis-1/12"
+        v-for="button in buttons"
+        :key="button.id"
+        :text="button.text"
+        @click="selectButton(button.id)"
+        :disabled="button.disable"
+      />
+      <ButtonsIconButton
+        class="basis-full w-icon-1 my-8 md:basis-1/12 md:my-0"
+        icon="heroicons:arrow-path-16-solid"
+        @click="resetColors()"
+      />
     </div>
     <section class="mt-8 flex flex-wrap justify-between items-center gap-4">
-      <section class="flex flex-col justify-between gap-4 grow basis-1/6 h-fit xl:h-full">
+      <section
+        class="flex flex-col justify-between gap-4 grow basis-1/6 h-fit xl:h-full"
+      >
         <template v-for="color in store.colors" key="color.id">
-          <div v-if="color.id === 5" class="flex justify-between items-center gap-4 w-full">
-            <ButtonsIconButton class="grow basis-1/12" icon="mdi:dice-3" @click="generate_foreground(color.id)" />
-            <ButtonsIconButton class="grow md:basis-1/12" icon="heroicons:arrow-up-16-solid"
-              @click="moreBright(color.id)" />
-            <InputsSimpleInput :id="'inputColor' + color.id" :placeholder="color.placeholder"
-              @getValue="(value) => updateFg(color.id, value)" :value="fg" />
+          <div
+            v-if="color.id === 5"
+            class="flex justify-between items-center gap-4 w-full"
+          >
+            <ButtonsIconButton
+              class="grow basis-1/12"
+              icon="mdi:dice-3"
+              @click="generate_foreground(color.id)"
+            />
+            <ButtonsIconButton
+              class="grow md:basis-1/12"
+              icon="heroicons:arrow-up-16-solid"
+              @click="moreBright(color.id)"
+            />
+            <InputsSimpleInput
+              :id="'inputColor' + color.id"
+              :placeholder="color.placeholder"
+              @getValue="(value: string) => updateFg(color.id, value)"
+              :value="fg"
+            />
           </div>
-          <div v-else-if="color.id === 6" class="flex justify-between items-center gap-4 w-full">
-            <ButtonsIconButton class="grow basis-1/12" icon="mdi:dice-3" @click="generate_background(color.id)" />
-            <ButtonsIconButton class="grow md:basis-1/12" icon="heroicons:arrow-down-16-solid"
-              @click="moreDark(color.id)" />
-            <InputsSimpleInput :id="'inputColor' + color.id" :placeholder="color.placeholder"
-              @getValue="(value) => updateBg(color.id, value)" :value="bg" />
+          <div
+            v-else-if="color.id === 6"
+            class="flex justify-between items-center gap-4 w-full"
+          >
+            <ButtonsIconButton
+              class="grow basis-1/12"
+              icon="mdi:dice-3"
+              @click="generate_background(color.id)"
+            />
+            <ButtonsIconButton
+              class="grow md:basis-1/12"
+              icon="heroicons:arrow-down-16-solid"
+              @click="moreDark(color.id)"
+            />
+            <InputsSimpleInput
+              :id="'inputColor' + color.id"
+              :placeholder="color.placeholder"
+              @getValue="(value: string) => updateBg(color.id, value)"
+              :value="bg"
+            />
           </div>
           <div v-else>
-            <InputsSimpleInput :id="'inputColor' + color.id" :placeholder="color.placeholder"
-              @getValue="(value) => updateColor(color.id, value)" />
+            <InputsSimpleInput
+              :id="'inputColor' + color.id"
+              :placeholder="color.placeholder"
+              @getValue="(value: string) => updateColor(color.id, value)"
+            />
           </div>
         </template>
-        <div class="flex w-full h-full rounded-xl bg-gray-200 dark:bg-gray-900"></div>
+        <div
+          class="flex w-full h-full rounded-xl bg-gray-200 dark:bg-gray-900"
+        ></div>
       </section>
-      <section class="flex flex-col grow basis-full rounded-xl overflow-hidden xl:basis-1/6 h-80 xl:h-full">
-        <div v-for="color in store.colors"
+      <section
+        class="flex flex-col grow basis-full rounded-xl overflow-hidden xl:basis-1/6 h-80 xl:h-full"
+      >
+        <div
+          v-for="color in store.colors"
           class="flex flex-auto transition-all duration-300 ease-out hover:py-4 hover:cursor-pointer active:py-8"
-          :style="bgGen(color.value)" @click="copyColor(color.value)"></div>
+          :style="bgGen(color.value)"
+          @click="copyColor(color.value)"
+        ></div>
       </section>
-      <section class="w-full h-fit xl:h-full flex justify-center items-center grow basis-full xl:basis-3/6">
+      <section
+        class="w-full h-fit xl:h-full flex justify-center items-center grow basis-full xl:basis-3/6"
+      >
         <OtherPreviewTheme />
       </section>
     </section>
-    <section class="flex flex-wrap justify-start items-center w-full h-full mt-20">
+    <section
+      class="flex flex-wrap justify-start items-center w-full h-full mt-20"
+    >
       <h1 class="text-3xl text-blue-500 w-full">Perfiles</h1>
-      <div v-for="profile in profiles" key="profile.id" class="mt-5 flex w-full justify-start items-start gap-8">
-        <span class="sm:w-1/6 text-xl text-gray-700 dark:text-gray-500">{{ profile.name }}</span>
+      <div
+        v-for="profile in profiles"
+        key="profile.id"
+        class="mt-5 flex w-full justify-start items-start gap-8"
+      >
+        <span class="sm:w-1/6 text-xl text-gray-700 dark:text-gray-500">{{
+          profile.name
+        }}</span>
         <div class="flex justify-start items-center gap-4">
-          <ButtonsIconButton class="w-icon-2" icon="heroicons:clipboard" @click="copyCode(profile.id)" />
+          <ButtonsIconButton
+            class="w-icon-2"
+            icon="heroicons:clipboard"
+            @click="copyCode(profile.id)"
+          />
         </div>
       </div>
     </section>
