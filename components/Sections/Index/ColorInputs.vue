54700ca4215e93d324890f81e46f04c76e32b089
HarmonySpace <jchavaspace@gmail.com>
2024-09-19 22:13:11 -0600

    style(components): more modular inputs
    separate inputs and create a components to have an input whit 2 events

--- components/Sections/Index/ColorInputs.vue
+++ components/Sections/Index/ColorInputs.vue
@@ -1,87 +1,49 @@
 <script setup lang="ts">
 const store = useColorsStore();
 const inputs = ref(
-  store.colors.map((color) => ({
-    id: color.id,
-    value: "",
-  })),
+	store.colors.map((color) => ({
+		id: color.id,
+		value: "",
+	})),
 );
 const updateColor = (id: number, color: string) => {
-  store.update(id, color);
+	store.update(id, color);
 };
 const updateInput = (id: number, color: string) => {
-  inputs.value[id - 1].value = color;
-  updateColor(id, color);
+	inputs.value[id - 1].value = color;
+	updateColor(id, color);
 };
 const generate_foreground = (id: number) => {
-  inputs.value[id - 1].value = colorGen("brighten");
-  updateColor(id, inputs.value[id - 1].value);
+	inputs.value[id - 1].value = colorGen("brighten");
+	updateColor(id, inputs.value[id - 1].value);
 };
 const generate_background = (id: number) => {
-  inputs.value[id - 1].value = colorGen("");
-  updateColor(id, inputs.value[id - 1].value);
+	inputs.value[id - 1].value = colorGen("");
+	updateColor(id, inputs.value[id - 1].value);
 };
 const moreBright = (id: number) => {
-  inputs.value[id - 1].value = brightenColor(inputs.value[id - 1].value, 0.1);
-  updateColor(id, inputs.value[id - 1].value);
+	inputs.value[id - 1].value = brightenColor(inputs.value[id - 1].value, 0.1);
+	updateColor(id, inputs.value[id - 1].value);
 };
 const moreDark = (id: number) => {
-  inputs.value[id - 1].value = darkenColor(inputs.value[id - 1].value, 0.1);
-  updateColor(id, inputs.value[id - 1].value);
+	inputs.value[id - 1].value = darkenColor(inputs.value[id - 1].value, 0.1);
+	updateColor(id, inputs.value[id - 1].value);
 };
 const resetColors = () => {
-  store.reset();
-  inputs.value.map((input) => {
-    input.value = "";
-  });
+	store.reset();
+	inputs.value.map((input) => {
+		input.value = "";
+	});
 };
 defineExpose({
-  resetColors,
+	resetColors,
 });
 </script>
 
 <template>
   <template v-for="color in store.colors" key="color.id">
-    <div
-      v-if="color.id === 5"
-      class="flex justify-between items-center gap-4 w-full"
-    >
-      <ButtonsIcon
-        class="basis-1/12 sm:h-8"
-        icon="mdi:dice-3"
-        @click="generate_foreground(color.id)"
-      />
-      <ButtonsIcon
-        class="basis-1/12 sm:h-8"
-        icon="heroicons:arrow-up-16-solid"
-        @click="moreBright(color.id)"
-      />
-      <InputsSimple
-        :placeholder="color.placeholder"
-        :text="inputs[color.id - 1].value"
-        @getText="(value: string) => updateInput(color.id, value)"
-      />
-    </div>
-    <div
-      v-else-if="color.id === 6"
-      class="flex justify-between items-center gap-4 w-full"
-    >
-      <ButtonsIcon
-        class="basis-1/12 sm:h-8"
-        icon="mdi:dice-3"
-        @click="generate_background(color.id)"
-      />
-      <ButtonsIcon
-        class="basis-1/12 sm:h-8"
-        icon="heroicons:arrow-down-16-solid"
-        @click="moreDark(color.id)"
-      />
-      <InputsSimple
-        :placeholder="color.placeholder"
-        :text="inputs[color.id - 1].value"
-        @getText="(value: string) => updateInput(color.id, value)"
-      />
-    </div>
+    <Inputs2actions v-if="color.id === 5" :placeholder="color.placeholder" icon1="mdi:dice-3" icon2="heroicons:arrow-up-16-solid" :text="inputs[color.id - 1].value" @getText="(value: string) => updateInput(color.id, value)" @action1="() => generate_foreground(color.id)" @action2="() => moreBright(color.id)" />
+    <Inputs2actions v-else-if="color.id === 6" :placeholder="color.placeholder" icon1="mdi:dice-3" icon2="heroicons:arrow-down-16-solid" :text="inputs[color.id - 1].value" @getText="(value: string) => updateInput(color.id, value)" @action1="() => generate_background(color.id)" @action2="() => moreDark(color.id)" />
     <div v-else>
       <InputsSimple
         :placeholder="color.placeholder"
