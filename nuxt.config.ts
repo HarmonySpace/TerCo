fe8713ffc438fbb4dd5c60c199040d5c28f9fb00
spicydogwings <spaceprotondev@proton.me>
2025-06-29 10:49:38 -0600

    feat(ui): tailwind agregado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
-    "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "@nuxtjs/color-mode",
     "nuxt-icon",
     "@vueuse/nuxt",
   ],
-
   colorMode: {
     classSuffix: "",
     preference: "light",
     fallback: "light",
   },
-
+  css: ['~/assets/css/main.css'],
   compatibilityDate: "2024-09-05",
+  vite: {    
+    plugins: [      
+      tailwindcss(),    
+    ],  
+  },
 });
