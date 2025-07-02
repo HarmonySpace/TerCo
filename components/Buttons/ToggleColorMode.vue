209bcf806c12aacbeaee147b96ad0d0f23a97c0d
spicydogwings <spaceprotondev@proton.me>
2025-07-01 22:38:45 -0600

    fix(ui): update icons size and some paddings on view

--- components/Buttons/ToggleColorMode.vue
+++ components/Buttons/ToggleColorMode.vue
@@ -2,22 +2,22 @@
 const emits = defineEmits(["click"]);
 const colorMode = useColorMode();
 const toggleColorMode = () => {
-  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
-  emits("click");
+    colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+    emits("click");
 };
 </script>
 
 <template>
-  <ButtonsIcon
-    @click="toggleColorMode"
-    v-show="colorMode.preference === 'light'"
-    class="grow w-icon-1 text-gray-600 transition-all duration-150 hover:text-gray-900 hover:cursor-pointer hover:scale-75"
-    icon="lucide:sun"
-  />
-  <ButtonsIcon
-    @click="toggleColorMode"
-    v-show="colorMode.preference === 'dark'"
-    class="grow w-icon-2 text-gray-600 transition-all duration-150 hover:text-gray-900 hover:cursor-pointer hover:scale-75"
-    icon="lucide:moon"
-  />
+    <ButtonsIcon
+        @click="toggleColorMode"
+        v-show="colorMode.preference === 'light'"
+        class="grow w-8 h-8 text-gray-600 transition-all duration-150 hover:text-gray-900 hover:cursor-pointer hover:scale-75"
+        icon="lucide:sun"
+    />
+    <ButtonsIcon
+        @click="toggleColorMode"
+        v-show="colorMode.preference === 'dark'"
+        class="grow w-8 h-8 text-gray-600 transition-all duration-150 hover:text-gray-900 hover:cursor-pointer hover:scale-75"
+        icon="lucide:moon"
+    />
 </template>
